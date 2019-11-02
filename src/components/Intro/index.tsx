c0e366ec9cb22ec09a3d1111a31fb9f18a3a915e
Carl Simpson <carl.simpson@zeninternet.co.uk>
2019-11-02 20:31:54 +0000

    title change

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -40,13 +40,13 @@ export const Intro: FunctionComponent<{}> = () => {
                                 effect="fade-slide-left"
                                 effectTimeout={timeoutBase}
                             >
-                                Grayscale promo
+                                Joe's Gardening Services
                             </Title>
                             <SubTitle
                                 effect="fade-slide-bottom"
                                 effectTimeout={timeoutBase + 500}
                             >
-                                GatsbyJS starter
+                                A helping hand for all those jobs you never get around to.
                             </SubTitle>
                         </Data>
                         <Arrow
